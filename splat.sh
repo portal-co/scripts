a660e08bd8184d936eb80af005c6234ef039d924
Graham Kelly <gkgoat6700@gmail.com>
2025-10-15 22:03:31 -0400

    fix splat

--- splat.sh
+++ splat.sh
@@ -2,4 +2,9 @@ D=$1
 mkdir -p "$D/.github/workflows"
 rm $D/.github/workflows/*
 cp $(dirname $0)/actions/* "$D/.github/workflows"
-cp $(dirname $0)/lint "$d/.github"
+cp $(dirname $0)/lint "$D/.github"
+
+pushd "$D"
+git add -A
+git commit -m "Update workflows"
+git push
